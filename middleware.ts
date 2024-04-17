c08c5bb2a17aa46996153dd38d698ce81a608a20
kenkenpa198 <kenkenpa198@gmail.com>
2024-04-17 23:42:59 +0900

    Middleware

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/mid-test1")) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  if (request.nextUrl.pathname.startsWith("/mid-test2")) {
+    return NextResponse.rewrite(new URL("/", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/mid-test1/:path*", "/(mid-test2|mid-test3)/:path*"],
+};
